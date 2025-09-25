55739f5961c7f9c55f673556d86196b56a101053
Ilia Mikheev <mikheev.show@gmail.com>
2025-09-25 21:01:05 +0300

    recreate hiddens

--- src/probing.py
+++
@@ -1,9 +0,0 @@
-import logging
-import logging_cofig
-
-from sklearn.linear_model import LogisticRegression
-
-logger = logging.getLogger(__name__)
-
-if __name__ == "__main__":
-    logger.info("Start ")
--- src/text_utils.py
+++ src/text_utils.py
@@ -17,12 +17,25 @@ def define_incongruent_word_start_index(congruent_sentence: str, incongruent_sen
     return [-1, -1]
 
 
+def find_last_word_indices(sentence: str) -> list[int]:
+    sentence = sentence.rstrip()
+    if not sentence:
+        raise ValueError("Sentence is empty")
+    last_space_index = sentence.rfind(' ')
+    start_index = last_space_index + 1 if last_space_index != -1 else 0
+    end_index = len(sentence) - 1
+
+    return [start_index, end_index]
+
+
 def find_index_of_incongruent_word(congruent_sentences: list[str], incongruent_sentences: list[str]) -> list[list[int]]:
     index_list: list[list[int]] = []
     if len(incongruent_sentences) != len(incongruent_sentences):
         raise ValueError("Incongruent sentences must have same length")
     for congruent_sentence, incongruent_sentence in zip(congruent_sentences, incongruent_sentences):
         i = define_incongruent_word_start_index(congruent_sentence, incongruent_sentence)
+        if i[0] == -1:
+            i = find_last_word_indices(congruent_sentence)
         index_list.append(i)
     return index_list
 
@@ -33,5 +46,10 @@ if __name__ == "__main__":
     correct_text = ["Автобусы проходят массовую дезинфекцию"]
     incorrect_text = ["Автобусы проходят массовую дезинфекцией"]
 
-    assert find_index_of_incongruent_word(correct_text, incorrect_text) == [27]
+    assert find_index_of_incongruent_word(correct_text, incorrect_text) == [[27, 39]]
+
+    correct_text = ["Автобусы проходят массовую дезинфекцию"]
+    incorrect_text = ["Автобусы проходят массовую дезинфекцию"]
+
+    assert find_index_of_incongruent_word(correct_text, incorrect_text) == [[27, 37]]
 
